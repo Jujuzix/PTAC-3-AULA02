31acbb17094fb23cab83a9c542d9afdfc506c720
Jujuzix <maronez.juliana@gmail.com>
2024-02-21 17:11:49 -0400

    Criação de componentes

---
+++ src/app/components/Rodape.js
@@ -0,0 +1,10 @@
+import './Todo.css';
+export default function Rodape(){
+    return(
+        <header>
+        <div class="pe">
+<p>Copyright © 1999 - 2021 GoDaddy Operating Company, LLC. Todos os direitos reservados.</p>
+        </div>
+        </header>
+    )
+}
